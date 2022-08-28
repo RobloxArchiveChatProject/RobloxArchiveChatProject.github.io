7365ae843d7e1485e389588d008a04bc1876719b
Algasami <105358814+algasami@users.noreply.github.com>
2022-08-28 21:17:03 +0800

    test

--- pages/cluster.tsx
+++ pages/cluster.tsx
@@ -29,18 +29,19 @@ const Cluster = () => {
             })
     }, [uuid])
 
+
     return <div className="w-screen">
         <Header branch="cluster" />
 
         <div className="flex w-screen flex-row justify-center mt-8"> {/* center alignment */}
             <div className="flex flex-col justify-center mx-8"> {/* main container */}
-                <h1 className={"text-3xl " + ((loading) ? "text-red-700 font-bold" : "")}> {currentUUID} </h1>
                 {
                     (!loading && dialogue)
-                        ? <>
+                        ? <div className={"p-4 rounded-2xl " + (dialogue.pkgversion === "0.0.2" ? "neon" : "")}>
+                            <h1 className={"text-3xl font-semibold " + ((loading) ? "text-red-700 font-bold" : "")}> {currentUUID} </h1>
                             <p>Game: <a className="link" href={"https://www.roblox.com/games/" + dialogue.game.toString()}>{dialogue.game}</a></p>
-                            <p>PKG Version: ${dialogue.pkgversion}</p>
-                            <p>Record Date: ${dialogue.date}</p>
+                            <p className={dialogue.pkgversion === "0.0.2" ? "dark:text-amber-600 text-orange-700 font-bold text-xl" : ""}>PKG Version: {dialogue.pkgversion}{dialogue.pkgversion === "0.0.2" ? " PUBLIC BETA!\u{1f973}" : ""}</p>
+                            <p>Record Date: {dialogue.date}</p>
                             <p>Owner: {createPlayerLink(dialogue.owner)}</p>
                             <h3>Users:</h3>
                             <ul className="list-decimal list-inside pl-3.5">
@@ -49,14 +50,14 @@ const Cluster = () => {
                                 })}
                             </ul>
                             <br />
-                            <div className="overflow-auto flex flex-col justify-start" style={{ maxHeight: "20rem" }}>
+                            <div className="overflow-auto scroll flex flex-col justify-start" style={{ maxHeight: "20rem" }}>
                                 {dialogue.data.map((v, i) => {
                                     return <div key={i} className="font-semibold px-2">
                                         <div className="badge"> {createPlayerLink(v.user, "d-link")}  </div> : {v.message}
                                     </div>
                                 })}
                             </div>
-                        </>
+                        </div>
                         : <div className="font-mono">
                             This can take up to ten seconds, depending on your Internet connection.
                         </div>
